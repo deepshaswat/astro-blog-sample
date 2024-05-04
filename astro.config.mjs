251e21a88d0757ae2158d07d4b8f3a703441909d
Shaswat Deep <deepshaswat@gmail.com>
2024-05-04 17:33:36 +0530

    Vercel Dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  //   site: "https://example.com",
   integrations: [preact()],
+  output: "server",
+  adapter: vercel(),
 });
