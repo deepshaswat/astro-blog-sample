4fc10428f6a5db4b3ff36bfc0641fc8fd692bbae
Shaswat Deep <deepshaswat@gmail.com>
2024-05-04 17:30:09 +0530

    Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "astro-blog-sample-k7tagoohu-shaswat-deeps-projects.vercel.app",
+  integrations: [preact()],
+});
